7cbd78c87eaccdcc5a6b63d8465b20962ffae5c4
swee meng <sweester@gmail.com>
2011-12-08 15:50:03 +0800

    added initial views and templates

---
+++ detail.tpl
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>{{bill.long_name}}</title>
+</head>
+<body>
+<div id='header'>
+</div>
+<body>
+<iframe src="{{revision.url}}"></iframe>
+</body>
+</body>
+</html>
---
+++ pages.py
@@ -0,0 +1,21 @@
+from bottle import route,view
+from models import bills,bill_revs
+from models import engine
+from sqlalchemy import select
+
+@route('/<id>/')
+@view('detail')
+def detail(id):
+    bl = select([bills],bills.c.id == id)
+    conn = engine.connect()
+    
+    result = conn.execute(bl)
+    bill = result.fetchone()
+    rev = select([bill_revs],bill_revs.c.bill_id == bill['id']).\
+        order_by(bill_revs.c.year.desc())
+    
+    result = conn.execute(rev)
+    revision = result.fetchone()
+    return dict(bill=bill,revision=revision)
+    
+    
