fc87e151a43986c0327822e0525c5071b7a2b6a6
sweemeng <sweester@gmail.com>
2011-12-29 15:40:44 +0800

    added more imports

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from bottle import run
 from pages import detail
 from pages import list_all
 from pages import about_us
+from pages import search
+from pages import feed
 from json_api import single_item
 from json_api import all_item
 
--- main_unicorn.py
+++ main_unicorn.py
@@ -1,9 +1,12 @@
 from bottle import run
 from pages import detail
 from pages import list_all
+from pages import about_us
+from pages import search
+from pages import feed
 from json_api import single_item
 from json_api import all_item
 
 
-run(server='gunicorn')
+run(server='gunicorn',port=8080)
 
