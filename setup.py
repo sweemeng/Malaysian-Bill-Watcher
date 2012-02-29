004f13ffdf77a09286ec42d8ab6b9a82614e1af8
sweemeng <sweester@gmail.com>
2012-02-29 15:41:00 +0800

    added basic setup.py buildout config, and bootstrap for environment setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = "billwatcher-malaysia",
+    version = "0.1",
+    url = "https://github.com/sweemeng/Malaysian-Bill-Watcher",
+    license = "GPL",
+    description = "A web app to make bills in parliament more visible, a sinar project",
+    author = "sweemeng",
+    packages = find_packages("billwatcher"),
+    package_dir = {'':'billwatcher'},
+    install_requires = [
+        'setuptools',
+        'bottle',
+        'pyes',
+        'SQLAlchemy',
+        'PyRSS2Gen',
+        'gevent',
+        'greenlet',
+        'gunicorn',
+        'httplib2',
+        'ipython',
+        'oauth2',
+        'python-twitter',
+        'simplejson',
+        'wsgiref'
+        ],
+)
