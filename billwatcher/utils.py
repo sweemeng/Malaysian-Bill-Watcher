d3ad1e1a1ba2500d5ff22ab8a7d3d60f065ecd75
sweemeng <sweester@gmail.com>
2012-03-08 14:50:01 +0800

    fixed bug for page number founding that causes wrong page num

--- billwatcher/utils.py
+++ billwatcher/utils.py
@@ -9,14 +9,23 @@ class Pagination(object):
             self.page_no = int(page_no)
         else:
             self.page_no = 1
+        self.extra_page = 0
         self.item_per_page = item_per_page
-        self.first = (self.page_no - 1)  *  self.item_per_page 
-        self.last = self.page_no * self.item_per_page
         self.total_item = total_item
-        self.page_list = range(self.total_item/self.item_per_page)
+        self.extra_item = self.total_item % self.item_per_page
+        if self.extra_item:
+            self.extra_page = 1 
+        self.total_page = (self.total_item / self.item_per_page) + self.extra_page
+        self.page_list = range(self.total_page)
         self.page_list = [i+1 for i in self.page_list]
         self.next_page = self.page_no + 1
         self.prev_page = self.page_no - 1
+        self.first = (self.page_no - 1)  *  self.item_per_page 
+        if self.page_no == self.page_list[-1] and self.extra_item:
+            self.last = self.total_item
+        else:
+            self.last = self.page_no * self.item_per_page
+
              
 
 
