69e95ee1e537f0f0120fe895fee2d3ca464eb6d7
sweemeng <sweester@gmail.com>
2012-03-16 14:45:29 +0800

    this is so we can override the variable fromsettings

--- billwatcher/settings.py
+++ billwatcher/settings.py
@@ -1,8 +1,3 @@
-try:
-    from settings_local import *
-except:
-    pass
-
 PORT = 8080
 HOST = "localhost"
 
@@ -11,3 +6,10 @@ DB_CONNECT = "sqlite:///data.db"
 URL = 'http://localhost:8080/'
 
 ITEM_PER_PAGE = 5
+
+# this is to override the variable from above
+try:
+    from settings_local import *
+except:
+    pass
+
