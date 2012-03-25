804cd31764da57f4bffa830e944883275df2eaf6
sweemeng <sweester@gmail.com>
2012-03-25 13:30:58 +0800

    add static views and host for unicorn server views

--- billwatcher/main_unicorn.py
+++ billwatcher/main_unicorn.py
@@ -8,6 +8,8 @@ from pages import search
 from pages import feed
 from json_api import single_item
 from json_api import all_item
+from static_views import css_views
+from static_views import js_views
 
 models.initdb()
-bottle.run(server='gunicorn', port=settings.PORT)
+bottle.run(server='gunicorn',host=settings.HOST,port=settings.PORT)
