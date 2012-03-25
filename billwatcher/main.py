726f93968b0316bcb52df186c6823c0cdb1f6703
sweemeng <sweester@gmail.com>
2012-03-25 13:30:36 +0800

    fix the simple main server, might remove it in the future

--- billwatcher/main.py
+++ billwatcher/main.py
@@ -6,7 +6,10 @@ from pages import search
 from pages import feed
 from json_api import single_item
 from json_api import all_item
+from static_views import css_views,js_views
 from settings import HOST,PORT
+from models import initdb
 
+initdb()
 run(host=HOST,port=PORT)
 
